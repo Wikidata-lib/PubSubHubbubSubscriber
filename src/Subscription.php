08b68075a6ceaa0809d9fbb851095bc30a8e63c8
Sebastian Brückner <sebastian.brueckner@student.hpi.uni-potsdam.de>
2014-04-10 10:43:06 +0200

    Added unsubscribe support to the DB.

--- src/Subscription.php
+++ src/Subscription.php
@@ -8,12 +8,14 @@ class Subscription {
 	private $mTopic;
 	private $mExpires;
 	private $mConfirmed;
+	private $mUnsubscribe;
 
-	public function __construct( $id = NULL, $topic = NULL, $expires = NULL, $confirmed = false ) {
+	public function __construct( $id = NULL, $topic = NULL, $expires = NULL, $confirmed = false, $unsubscribe = false ) {
 		$this->mId = $id;
 		$this->mTopic = $topic;
 		$this->mExpires = $expires;
 		$this->mConfirmed = (bool) $confirmed;
+		$this->mUnsubscribe = (bool) $unsubscribe;
 	}
 
 	/**
@@ -25,7 +27,7 @@ class Subscription {
 	public static function findByTopic( $topicURL ) {
 		$dbr = wfGetDB( DB_SLAVE );
 		$result = $dbr->select( 'push_subscriptions',
-			array( 'psb_id', 'psb_expires', 'psb_confirmed' ),
+			array( 'psb_id', 'psb_expires', 'psb_confirmed', 'psb_unsubscribe' ),
 			array( 'psb_topic' => $topicURL ) );
 
 		if ( $result->numRows() == 0 ) {
@@ -33,15 +35,32 @@ class Subscription {
 		}
 
 		$data = $result->fetchObject();
-		return new Subscription( $data->psb_id, $topicURL, $data->psb_expires, $data->psb_confirmed );
+		return new Subscription(
+			$data->psb_id,
+			$topicURL,
+			$data->psb_expires,
+			$data->psb_confirmed,
+			$data->psb_unsubscribe
+		);
 	}
 
 	public function update() {
 		$dbw = wfGetDB( DB_MASTER );
 		if ( $this->mId ) {
 			$dbw->update( 'push_subscriptions',
-				array( 'psb_expires' => $this->mExpires, 'psb_confirmed' => $this->mConfirmed ),
+				array(
+					'psb_expires' => $this->mExpires,
+					'psb_confirmed' => $this->mConfirmed,
+					'psb_unsubscribe' => $this->mUnsubscribe,
+				),
 				array( 'psb_id' => $this->mId ) );
+		}
+	}
+
+	public function delete() {
+		$dbw = wfGetDB( DB_MASTER );
+		if ( $this->mId ) {
+			$dbw->delete( 'push_subscriptions', array( 'psb_id' => $this->mId ) );
 		}
 	}
 
@@ -51,6 +70,14 @@ class Subscription {
 
 	public function setConfirmed( $confirmed ) {
 		$this->mConfirmed = (bool) $confirmed;
+	}
+
+	public function isUnsubscribed() {
+		return $this->mUnsubscribe;
+	}
+
+	public function setUnsubscribed( $unsubscribe ) {
+		$this->mUnsubscribe = (bool) $unsubscribe;
 	}
 
 }
