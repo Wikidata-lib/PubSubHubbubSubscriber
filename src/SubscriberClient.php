a16142bd1adbbb836979309e1cbea8be01590a07
Sebastian Brückner <sebastian.brueckner@student.hpi.uni-potsdam.de>
2014-04-16 16:16:32 +0200

    Refactored Link header extraction.

--- src/SubscriberClient.php
+++ src/SubscriberClient.php
@@ -14,7 +14,8 @@ class SubscriberClient {
 	}
 
 	public function subscribe() {
-		$linkHeaders = self::findLinkHeaders( $this->mResourceURL );
+		$rawLinkHeaders = self::findRawLinkHeaders( $this->mResourceURL );
+		$linkHeaders = self::parseLinkHeaders( $rawLinkHeaders );
 		$hubURL = $linkHeaders['hub'];
 		$this->mResourceURL = $linkHeaders['self'];
 
@@ -24,13 +25,28 @@ class SubscriberClient {
 		self::sendSubscriptionRequest( $hubURL, $this->mResourceURL );
 	}
 
-	private static function findLinkHeaders( $resourceURL ) {
+	/**
+	 * Retrieve raw link headers from the given URL.
+	 *
+	 * @param string $resourceURL The resource's URL.
+	 * @return string[] an indexed array containing values of all HTTP Link headers.
+	 */
+	private static function findRawLinkHeaders( $resourceURL ) {
 		$req = MWHttpRequest::factory( $resourceURL, array(
 			'method' => 'HEAD',
 		) );
 		$req->execute();
 		$rawLinkHeaders = $req->getResponseHeaders();
-		$rawLinkHeaders = $rawLinkHeaders['link'];
+		return $rawLinkHeaders['link'];
+	}
+
+	/**
+	 * Parse raw HTTP Link headers.
+	 *
+	 * @param string[] $rawLinkHeaders An indexed array of raw HTTP Link headers as returned by findRawLinkHeaders.
+	 * @return string[] an associative array with the link rel as key and the URL as value.
+	 */
+	private static function parseLinkHeaders( $rawLinkHeaders ) {
 		$linkHeaders = array();
 		foreach ( $rawLinkHeaders as $rawLinkHeader ) {
 			$count = preg_match_all( "/<(?<url>[^>]+)>;\\s*rel=\"(?<rel>hub|self)\"/", $rawLinkHeader, $matches );
